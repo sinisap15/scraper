c6cec6984d29b57a47594e9cc31d7c0ad845768c
Siniša Panić <sinisa.panic@gamanza.com>
2024-05-31 15:06:21 +0200

    Feature: download fonts and refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+const fs = require("fs");
+const ScraperService = require("./service/service");
+require("dotenv").config();
+
+const baseUrl = process.env.BASE_URL;
+const outputDir = process.env.OUTPUT_DIR;
+let scraperService;
+async function startServices() {
+  scraperService = new ScraperService();
+}
+
 (async () => {
-  console.log("Starting app");
-  console.log("Shutting down!");
-})().catch(error => {
-  logger.error("Initialization error", error);
-  process.exit(1);
-});
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+  await startServices();
+  await scraperService.downloadCSS(baseUrl);
+  await scraperService.downloadFavicons(baseUrl);
+  await scraperService.downloadScripts(baseUrl);
+  await scraperService.downloadCategories(baseUrl);
+  await scraperService.downloadFonts(baseUrl);
+  await scraperService.scrapePages(baseUrl);
+  console.log("Scraping complete!");
+})();
